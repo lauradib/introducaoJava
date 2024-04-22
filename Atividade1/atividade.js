3233d810d82297f3fad8169482b8c70ecb583716
lauradib <diblaura22@gmail.com>
2024-04-22 11:47:23 -0300

    atividades

--- Atividade1/atividade.js
+++ Atividade1/atividade.js
@@ -1,6 +1,7 @@
 
 
-function soma (){
+function atividade1(){
+    window.alert("ATIVIDADE 1")
     let valor = Number(window.prompt("Digite o valor da conta:"));
     let clientes = Number(window.prompt("Digite o numero de clientes:"));
     console.log (typeof valor)
@@ -11,4 +12,12 @@ function soma (){
     window.alert ("O valor para cada um é de:" +resultados)
 }
 
-soma()
+
+function atividade2(){
+    window.alert("Digite um algoritimo:")
+    let numero = Number(window.prompt("Digite o primeiro numero:"));
+
+    window.alert (`O numero inserido é ${numero},  sucessor é ${numero=+1} e seu antecessor é de ${numero-1}`)
+
+
+}
